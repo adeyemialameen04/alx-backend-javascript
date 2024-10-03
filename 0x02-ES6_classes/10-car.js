25848d393b9f195424ec745e85d0531ff6bf1c55
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-03 18:32:53 +0100

    car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  get brand() {
+    return this[_brand];
+  }
+
+  get motor() {
+    return this[_motor];
+  }
+
+  get color() {
+    return this[_color];
+  }
+
+  cloneCar() {
+    const Constructor = this.constructor[Symbol.species] || this.constructor;
+    return new Constructor();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}
