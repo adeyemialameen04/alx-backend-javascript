837edc0b25a0d1d6b24e92681541933a63b7f60d
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-08 14:30:17 +0100

    tasks

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+// 4-update_grade_by_city.js
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
