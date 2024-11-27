f44d17c822c04b7fed1220f27d5ac20863ac74a1
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-11-27 20:11:22 +0100

    8-1pi

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,12 +1,12 @@
+// import express from 'express';
 const express = require('express');
 
+const port = 7865;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
-
-const port = 7865;
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
