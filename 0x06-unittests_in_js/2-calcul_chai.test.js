80764688417283de12e81d1fd0ff935e92bf7d54
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-11-27 19:58:15 +0100

    2 calchul

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,33 +1,23 @@
-/* eslint-disable jest/valid-expect */
-/* eslint-disable jest/expect-expect */
-/* eslint-disable jest/prefer-expect-assertions */
-const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require("./2-calcul_chai")
+const chai = require("chai");
 
-// You can assume a and b are always number
+const expect = chai.expect;
 
-describe('calculateNumber', () => {
-  it('handles adding two rounded numbers when type is SUM', () => {
-    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-    expect(calculateNumber('SUM', -1.2, -3.7)).to.equal(-5);
+describe("calculateNumber", function () {
+  it("checks sum", function () {
+    const test = calculateNumber('SUM', 1.4, 4.5);
+    expect(test).to.equal(6);
   });
-
-  it('handles subtracting two rounded numbers when type is SUBTRACT', () => {
-    expect(calculateNumber('SUBTRACT', 5.9, 2.3)).to.equal(4);
-    expect(calculateNumber('SUBTRACT', -5.9, -2.3)).to.equal(-4);
+  it("checks subtract", function () {
+    const test = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(test).to.equal(-4);
   });
-
-  it('handles dividing two rounded numbers when type is DIVIDE', () => {
-    expect(calculateNumber('DIVIDE', 8, 2)).to.equal(4);
-    // expect(calculateNumber('DIVIDE', 5.0, 3.0)).toEqual(2);
+  it("checks divide", function () {
+    const test = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(test).to.equal(0.2);
   });
-
-  it('returns Error when dividing by 0', () => {
-    expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
-    expect(calculateNumber('DIVIDE', -8, 0)).to.equal('Error');
-  });
-
-  it('throws an error when an invalid type is provided', () => {
-    expect(() => calculateNumber('INVALID_TYPE', 1, 2)).to.throw(Error);
+  it("checks division by 0", function () {
+    const test = calculateNumber('DIVIDE', 1.4, 0);
+    expect(test).to.equal('Error');
   });
 });
